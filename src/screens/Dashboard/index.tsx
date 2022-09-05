ce5b9f460a96254bd369f953112b4e535d8cb422
Ester Souza <ester@Mac-mini-de-Ester.local>
2022-09-05 14:05:16 -0300

    First commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from "./styles"
+
+export function Dashboard() {
+    return (
+        <Container >
+            <Header> 
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{
+                            uri: 'https://avatars.githubusercontent.com/u/57070379?v=4'}}
+                        />
+                        <User>
+                            <UserGreeting>Olá</UserGreeting>
+                            <UserName>Ester</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name='power'/>
+                </UserWrapper>
+            </Header>
+        </Container>
+    )
+}
